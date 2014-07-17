746472338dc7cb999d4c60ea1476f2bad7e1919a
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-07-17 14:18:32 -0700

    refine the move method to return a new position when a hero is stepping or turning

--- lib/map.rb
+++ lib/map.rb
@@ -1,8 +1,11 @@
 require 'matrix'
 
 class Map
+
   LEFT_TURN = {"N" => "W", "S" => "E", "E" => "N", "W" =>"N"}
   RIGHT_TURN = {"N" => "E", "S" => "W", "E" => "S", "W" =>"S"}
+  NEXT_STEP = {"N" => :step_ahead, "S" =>:step_behind, "E" => :step_right, "W" => :step_left}
+  STEP_SIZE = 1
 
   def initialize(x,y)
     @map = Matrix.build(x,y) {0}
@@ -15,7 +18,29 @@ class Map
     when "R"
       position.merge(dir: RIGHT_TURN[position[:dir]])
     when "M"
+      send(NEXT_STEP[position[:dir]], position)
     end
   end
 
+  private
+
+  def step_ahead(position)
+    position[:y] += STEP_SIZE
+    position
+  end
+
+  def step_behind(position)
+    position[:y] -= STEP_SIZE
+    position
+  end
+
+  def step_left(position)
+    position[:x] -= STEP_SIZE
+    position
+  end
+
+  def step_right(position)
+    position[:x] += STEP_SIZE
+    position
+  end
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/map.rb"
 
 describe Map do
-  context "hero is facing north at (1,1)" do
-    describe "move" do
+  describe "move" do
+    context "hero is facing north at (1,1) and turning" do
       let(:position) { {x: 1, y: 1, dir: "N"} }
 
       it "updates the position to west if the next move if left" do
@@ -15,6 +15,35 @@ describe Map do
         expect(map.move("R", position)).to eq ({x: 1, y: 1, dir: "E" })
       end
 
+
+    end
+
+    context "hero is at (1,1) and stepping " do
+      let(:position) { {x: 1, y: 1} }
+
+      it "increments y coordinate by 1 if hero is facing north" do
+        position[:dir] = "N"
+        map = Map.new(5,5)
+        expect(map.move("M", position)).to eq ({x: 1, y: 2, dir: "N" })
+      end
+
+       it "decrements y coordinate by 1 if hero is facing south" do
+        position[:dir] = "S"
+        map = Map.new(5,5)
+        expect(map.move("M", position)).to eq ({x: 1, y: 0, dir: "S" })
+      end
+
+       it "increments x coordinate by 1 if hero is facing east" do
+        position[:dir] = "E"
+        map = Map.new(5,5)
+        expect(map.move("M", position)).to eq ({x: 2, y: 1, dir: "E" })
+      end
+
+       it "increments y coordinate by 1 if hero is facing north" do
+        position[:dir] = "W"
+        map = Map.new(5,5)
+        expect(map.move("M", position)).to eq ({x: 0, y: 1, dir: "W" })
+      end
     end
   end
 end
