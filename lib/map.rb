963398a098f08fcedf5158a4267cbe7c8ccf4331
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-07-19 15:45:18 -0700

    Forbid hero from venturing out of the map

--- lib/map.rb
+++ lib/map.rb
@@ -8,11 +8,18 @@ require_relative "./direction_east.rb"
 class Map
 
   def initialize(x,y)
-    @map = Matrix.build(x,y) {0}
+    @x_boundary = x
+    @y_boundary = y
   end
 
   def next_position(position)
-    new_position(position)
+    next_position = new_position(position)
+    if out_of_bound?(next_position)
+      p "End of map reached."
+      position
+    else
+      next_position
+    end
   end
 
   private
@@ -29,5 +36,9 @@ class Map
     when DirectionWest.instance
       position.merge(x: x - 1)
     end
+  end
+
+  def out_of_bound?(position)
+    position[:x] > @x_boundary || position[:y] > @y_boundary
   end
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -5,7 +5,7 @@ require_relative "../lib/direction_west.rb"
 require_relative "../lib/direction_east.rb"
 
 describe Map do
-  describe "position" do
+  describe "next_position" do
     let(:position) { {x: 1, y: 2} }
     it "increases x coordinate by 1 if hero is facing east" do
       map = Map.new(5,5)
@@ -26,5 +26,11 @@ describe Map do
      map = Map.new(5,5)
      expect(map.next_position(position.merge(dir: DirectionSouth.instance))).to eq ({x: 1, y: 1, dir: DirectionSouth.instance })
    end
- end
+
+   it "does not allow the hero to move if next position is invalid" do
+    map = Map.new(5,5)
+    edge_of_map = {x: 5, y: 1, dir: DirectionEast.instance}
+    expect(map.next_position(edge_of_map)).to eq edge_of_map
+  end
+end
 end
